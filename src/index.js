50b64f70bf3a4262c8461394abb343fd293ba679
faridvl <faridvl095.fv@gmail.com>
2021-10-27 01:18:02 -0600

    08-exports and imports, use filter and find

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import {heroes} from './data/heroes';
+
+console.log(heroes);
+//USO DEL FIND
+const getHeroeById = (id)=> {
+    return heroes.find( (value)=> value.id = id )
+}
+
+
+//USO DEL FILTER
+const getHeroesByOwner = ( owner ) => heroes.filter( (heroes) => heroes.owner === owner )
+
+console.log(getHeroeById(3));
+console.log(getHeroesByOwner('Marvel'));
